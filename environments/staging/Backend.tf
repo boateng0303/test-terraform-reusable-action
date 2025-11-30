ebf9516d7388f20e9f8c5eb817165ee7627cc1f8
boateng0303 <boatengkwasi0303@gmail.com>
2025-11-30 16:54:24 +0000

    Update Backend.tf

--- environments/staging/Backend.tf
+++ environments/staging/Backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "kay0303-bucket"
+    bucket = "kay0303"
     key    = "staging/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-locks"   # optional but recommended
